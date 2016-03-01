ab2657121668301cb661c7112b2cedebb2ca38ff
moto <mthrok@gmail.com>
2016-03-01 00:05:39 -0800

    Solved inod

---
+++ problems/inod.py
@@ -0,0 +1,32 @@
+from __future__ import print_function
+
+
+def preprocess(string):
+    return int(string.strip())
+
+
+def solve(n):
+    return n-2
+
+
+def parse_command_line_arguments():
+    import os
+    default_input = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)), 'dataset',
+        'rosalind_{}.txt'.format(__file__.split('/')[-1].split('.')[0]))
+    from argparse import ArgumentParser as AP
+    ap = AP()
+    ap.add_argument('--input', default=default_input)
+    return ap.parse_args()
+
+
+def main():
+    args = parse_command_line_arguments()
+    data = preprocess(open(args.input, 'r').read())
+    print('*INPUT*\n{}\n'.format(data))
+    ret = solve(data)
+    print('*OUTPUT*\n{}'.format(ret))
+
+
+if __name__ == '__main__':
+    main()
